cd9ac2d7ed4bd19702920ca026dd551a5ae085fc
Evgeniy Popelyaev <webpixel-development@yandex.ru>
2025-04-16 13:57:04 +0700

    Добавил раочий плагин для обертки html и не только - surround.mini

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,9 +1,19 @@
 return {
-   "kylechui/nvim-surround",
-   version = "3.0.0", -- Use for stability; omit to use `main` branch for the latest features
-   -- event = "VeryLazy",
-   config = function()
-      require("nvim-surround").setup({
-      })
-   end,
+    "echasnovski/mini.surround",
+    version = "*",
+    config = function()
+        require("mini.surround").setup({
+            mappings = {
+                add = "sa", -- Добавить окружение: sa<окружение>
+                delete = "sd", -- Удалить: sd<окружение>
+                find = "sf", -- Найти справа
+                find_left = "sF", -- Найти слева
+                highlight = "sh", -- Подсветка
+                replace = "sr", -- Заменить окружение
+                update_n_lines = "sn", -- Обновить количество строк
+                suffix_last = "l", -- К примеру: sal — последнее
+                suffix_next = "n", -- san — следующее
+            },
+        })
+    end,
 }
